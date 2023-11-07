e65f11d9305409b7c50a03b850f25374fe50d833
marcomacaluso3 <marco.macaluso@incuto.com>
2023-11-07 21:59:25 +0000

    user now able to save posts to micro_post table

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,13 +2,14 @@
 // THIS IS THE CONTROLLER
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
-use DateTime;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
 {
@@ -66,11 +67,12 @@ class MicroPostController extends AbstractController
      *
      * This method displays a form for adding a new MicroPost. It creates a new MicroPost object, 
      * generates a form for it, and then renders the form view for user input.
-     *
+     * 
      * @return Response A Response object representing the form view for adding a new MicroPost.
      */
-    #[Route('micro-post/addPost', name: 'app_micro_post_addPost', priority : 2 )]
-    public function addPost(): Response {
+    #[Route('/micro-post/add', name: 'app_micro_post_addPost', priority : 2 )]
+    // adding "$posts" as a 2nd param injects it to give access to the MicroPostRepository repository
+    public function add(Request $request, MicroPostRepository $posts): Response {
 
         // create a new object of the MicroPost class
         $microPost = new MicroPost();
@@ -84,6 +86,24 @@ class MicroPostController extends AbstractController
         ->add('submit', SubmitType::class, ['label' => 'Save post'])
         // gets the form
         ->getForm();
+
+        // calls the link submit() method if form was successfully submitted 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new Datetime());
+            /* this calls the add() method from src\Repository\MicroPostRepository.php
+            and that repository is available because it is injected as the 2nd param of
+            when calling this method.
+            This repository contains the add() and remove() methods which do the actual
+            updating of the database. They call those methods, pass "entities" as the
+            params. NOTE: an entity is a class that represents a table in a db.*/
+            $posts->add($post, true);
+            // adds user alert
+
+            // Redirect to another page
+        }
 
         // returns by using render() function, first param is what View (V of MVC) to show (aka what twig template)
         return $this->renderForm(
