91b051186cc6b1f9cbe125629ec0cfb35112c01e
marcomacaluso3 <marco.macaluso@incuto.com>
2023-12-12 19:27:58 +0000

    added form validation

--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\MicroPostRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MicroPostRepository::class)]
 class MicroPost
@@ -14,10 +15,17 @@ class MicroPost
     #[ORM\Column]
     private ?int $id = null;
 
+    /* because these (title and text) don't have a nullable param, symfony assumes they are required fields 
+    which means their input fields on an html form will automatically have the required attr added */
     #[ORM\Column(length: 255)]
+    // basically calls the Assert (validator) obj and calls its NotBlank() method
+    #[Assert\NotBlank()]
+    // calls the Length() method to check for input length 
+    #[Assert\Length(min: 1, max: 255)]
     private ?string $title = null;
 
     #[ORM\Column(length: 500)]
+    #[Assert\Length(min: 1, max: 500, maxMessage: 'Please limit your post text to fewer than 500 characters.')]
     private ?string $text = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
